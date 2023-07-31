d647de41d2c1ae48f77c452f5a21fbc8331a865f
elephant97 <elephant0908@gmail.com>
2023-08-01 01:01:53 +0900

    프로젝트 재 구성

---
+++ src/test/java/com/example/nonbak/autotasker/AutoTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nonbak.autotasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
