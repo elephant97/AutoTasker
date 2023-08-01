d2bb61f553409990851777710aa6d49974ce9a41
elephant97 <elephant0908@gmail.com>
2023-08-02 00:43:38 +0900

    -yaml파일 타임리프 추가 -/admin url 요청 시 컨트롤러로 admin에 해당하는 html로 연결되도록 추가 -admin 페이지 메인으로 사용할 admin-main.html 파일 추가   -admin-main.html user list 출력 할 기본 테이블 틀 작성(bootstrap적용) -파일 디렉토리 구조 변경 (필요없는 폴더 삭제)

--- src/main/java/com/autotasker/AutoTaskerApplication.java
+++ src/main/java/com/autotasker/AutoTaskerApplication.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class AutoTaskerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AutoTaskerApplication.class, args);
     }
+
 }
---
+++ src/main/java/com/autotasker/service/admin/UserManageService.java
@@ -0,0 +1,6 @@
+package com.autotasker.service.admin;
+
+public class UserManageService {
+
+
+}
