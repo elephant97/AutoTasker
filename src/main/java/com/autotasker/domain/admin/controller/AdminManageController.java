95404850969174befbcc0fecffc4c34e98fb4b30
elephant97 <elephant0908@gmail.com>
2023-10-30 14:24:33 +0900

    - port 8081로 변경 - AdminManageController에서 하위 경로 추가할 수 있도록 Requestmapping 추가 - status가 N인경우 승인 및 거절할 수 있도록 버튼추가 - 버튼에 스타일 적용할 수 있도록 css 추가

--- src/main/java/com/autotasker/domain/admin/controller/AdminManageController.java
+++ src/main/java/com/autotasker/domain/admin/controller/AdminManageController.java
@@ -9,16 +9,18 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 //@RequestMapping //TODO admin User일 경우 admin 페이지 버튼이 활성화 되고, 해당 페이지로 매핑되도록 변경예정
+@RequestMapping("/admin")
 @Controller
 public class AdminManageController {
     //TODO user 정보 List가 출력되도록 해당 서비스 구현예정
     @Autowired
     UserListRepository userListRepository;
 
-    @GetMapping(value = "/admin")
+    @GetMapping
     public String AdminGetUserList(@RequestParam(name = "page", defaultValue = "0") int page, Model model)
     {
         int pageSize = 5; // 한 페이지에 표시할 항목 수
@@ -29,7 +31,7 @@ public class AdminManageController {
         return "admin/user-manage";
     }
 
-    @GetMapping(value ="/admin/UserJoin")
+    @GetMapping(value ="/UserJoin")
     public String AdminRequestUserJoin(Model model)
     {
         return "admin/UserJoin";
